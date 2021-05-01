c81dbb539ccbec88ea855c4b286960b9b3a0f7b3
Xabier <xabierbahillo1@gmail.com>
2021-05-02 00:15:01 +0200

    Implementado inicio de sesion

--- app/src/main/java/com/das/preguntados/Activitys/LoginActivity.java
+++ app/src/main/java/com/das/preguntados/Activitys/LoginActivity.java
@@ -20,8 +20,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.das.preguntados.Common.generateAuthToken;
+import com.das.preguntados.Dialogs.DialogMessage;
 import com.das.preguntados.R;
 import com.das.preguntados.WS.comprobarTokenAuthWS;
+import com.das.preguntados.WS.iniciarSesionWS;
 import com.das.preguntados.WS.registroWS;
 
 public class LoginActivity extends AppCompatActivity {
@@ -32,7 +34,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
         gestionarInicioSesionAutomatico();
-
         //Gestion de eventos
         gestionarRegisterText();
         gestionarLoginButton();
@@ -44,7 +45,6 @@ public class LoginActivity extends AppCompatActivity {
         SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(generateAuthToken.AUTH_CONTEXT, Context.MODE_PRIVATE); //Obtengo las preferencias de autenticacion
         String usuario= sharedPref.getString("usuario",null); //Obtengo el usuario de las preferencias, si no existe devuelvo null
         String token= sharedPref.getString("token",null); //Obtengo el token de las preferencias, si no existe devuelvo null
-
         if (usuario!=null && token!=null){ //Si ambos no son nulos
             //Lanzo una peticion contra la base de datos para comprobar si son datos correctos
             Log.d("inicioSesionAutomatico","Se ha encontrado sesion para el usuario: "+usuario);
@@ -95,18 +95,6 @@ public class LoginActivity extends AppCompatActivity {
         }
 
     }
-    private void gestionarInicioSesion(String usuario, String clave){
-        /*TODO: Inicio de sesion manual
-          Genero un token de alguna manera para guardar el login, y envio a base de datos usuario, clave y el token.
-          Si es login correcto, guardo el token en la base de datos y en sharedPreferences, finalizo esta actividad y paso a
-          la actividad principal.
-        */
-
-        finish(); //Finalizo mi actividad
-        Intent i = new Intent(getApplicationContext(), MainMenuActivity.class);
-        i.putExtra("usuario","admin"); //Por defecto le paso un usuario administrador
-        startActivity(i);
-    }
 
     private void gestionarRegisterText(){
         //Gestiona los eventos del textView registerText
@@ -132,9 +120,93 @@ public class LoginActivity extends AppCompatActivity {
                 EditText passwordET=findViewById(R.id.claveText);
                 String usuario=usuarioET.getText().toString();
                 String password=passwordET.getText().toString();
-                gestionarInicioSesion("admin","password");
+                if (usuario.isEmpty() || password.isEmpty()){ //Si alguno de los campos es vacio, muestro mensaje de error
+                    gestionarError(getString(R.string.login_error_camposObligatorios));
+                }
+                else {
+                    gestionarInicioSesion(usuario,password);
+                }
             }
         });
     }
 
+    private void gestionarInicioSesion(String usuario, String clave){
+        // Inicio de sesion manual
+         /* Genero un token de alguna manera para guardar el login, y envio a base de datos usuario, clave y el token.
+          Si es login correcto, guardo el token en la base de datos y en sharedPreferences, finalizo esta actividad y paso a
+          la actividad principal.
+        */
+        //Genero un token para guardar el login
+        String token=generateAuthToken.generateToken();
+        //Peticion contra BD para comprobar el login
+        Log.d("login","Verificaciones correctas. Comienza petición a base de datos para login");
+
+        Data datos = new Data.Builder()
+                .putString("usuario",usuario)
+                .putString("clave",clave)
+                .putString("token",token)
+                .build();
+
+        OneTimeWorkRequest iniciarSesionOtwr= new OneTimeWorkRequest.Builder(iniciarSesionWS.class).setInputData(datos)
+                .build();
+
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(iniciarSesionOtwr.getId())
+                .observe(this, new Observer<WorkInfo>() {
+                    @Override
+                    public void onChanged(WorkInfo workInfo) {
+                        //Trato la respuesta, teniendo en cuenta que es de la forma CODIGO#VALOR
+                        if(workInfo != null && workInfo.getState().isFinished()){
+
+                            String resultado=workInfo.getOutputData().getString("resultado");
+                            if (resultado!=null) {
+                                String[] respuesta= resultado.split("#");
+                                if (respuesta[0].equals("ERR")){ //Hay un error
+                                    gestionarError(gestionarMensajesError(respuesta[1]));
+                                }
+                                else if (respuesta[0].equals("OK")){
+                                    //Datos de login correctos, guardo token en preferencias y lanzo la actividad principal
+                                    Log.d("login","Iniciada sesion con usuario"+usuario);
+                                    SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(generateAuthToken.AUTH_CONTEXT, Context.MODE_PRIVATE); //Obtengo las preferencias de autenticacion
+                                    SharedPreferences.Editor editor = sharedPref.edit();
+                                    editor.putString("usuario", usuario);
+                                    editor.putString("token",token);
+                                    editor.commit();
+                                    finish(); //Finalizo actividad actual
+                                    Intent i = new Intent(getApplicationContext(), MainMenuActivity.class);
+                                    i.putExtra("usuario",usuario); //Envio los datos del usuario
+                                    startActivity(i);
+                                }
+                            }
+
+                        }
+                    }
+                });
+        WorkManager.getInstance(getApplicationContext()).enqueue(iniciarSesionOtwr);
+    }
+
+    private void gestionarError(String message){
+        //Muestra un dialog con el error indicado
+        DialogFragment dialogoError= DialogMessage.newInstance(getString(R.string.login_alertTitle),message);
+        dialogoError.show(getSupportFragmentManager(), "errorLogin");
+        //Muestro el error en los logs
+        Log.d("login",message);
+    }
+
+    private String gestionarMensajesError(String message){
+        //Gestiona los mensajes de error recibidos en la respuesta
+        if (message.equals("login_error_conexionBD")) {
+            return getString(R.string.login_error_conexionBD);
+        }
+        if (message.equals("login_error_consultaBD")){
+            return getString(R.string.login_error_consultaBD);
+        }
+        if (message.equals("login_error_camposObligatorios")){
+            return getString(R.string.login_error_camposObligatorios);
+        }
+        if (message.equals("login_error_authIncorrecta")) {
+            return getString(R.string.login_error_authIncorrecta);
+        }
+
+        return "Ha sucedido un error al iniciar sesion";
+    }
 }
